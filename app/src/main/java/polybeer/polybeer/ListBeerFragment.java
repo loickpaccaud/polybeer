14755720e53bca0512d45f4639564a6dbdc793e1
unknown <loick.paccaud@gmail.com>
2018-01-15 19:36:42 +0100

    debut api

--- app/src/main/java/polybeer/polybeer/ListBeerFragment.java
+++ app/src/main/java/polybeer/polybeer/ListBeerFragment.java
@@ -6,7 +6,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.toolbox.JsonArrayRequest;
 
@@ -14,9 +13,14 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import polybeer.polybeer.Object.Beer;
 
 public class ListBeerFragment extends Fragment{
+
+    private List<Beer> beerList;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -24,26 +28,60 @@ public class ListBeerFragment extends Fragment{
     }
 
     private void listBeer(){
-        String api_url = "https://api.punkapi.com/v2/beers";
-        Beer beer = new Beer();
+        final String api_url = "https://api.punkapi.com/v2/beers";
+        final Beer beer = new Beer();
+        beerList = new ArrayList<>();
 
-        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest (Request.Method.GET, api_url, null, new Response.Listener<JSONArray>() {
-                    @Override
-                    public void onResponse(JSONArray response) {
-                        try{
-                            for(int i=0 ; i<response.length(); i++){
-                                JSONObject jsonObject = response.getJSONObject(i);
-                                int beer_id = jsonObject.getInt("id");
-                            }
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest (api_url, new Response.Listener<JSONArray>() {
+
+            @Override
+            public void onResponse(JSONArray response) {
+                try{
+                    for(int i=0 ; i<response.length(); i++){
+                        JSONObject jsonObject = response.getJSONObject(i);
+                        int id = jsonObject.getInt("id");
+                        String name = jsonObject.getString("name");
+                        String tagline = jsonObject.getString("tagline");
+                        String first_brewed = jsonObject.getString("first_brewed");
+                        String description = jsonObject.getString("description");
+                        String image_url = jsonObject.getString("image_url");
+                        Double abv = jsonObject.getDouble("abv");
+                        Double ibu = jsonObject.getDouble("ibu");
+                        Double target_fg = jsonObject.getDouble("target_fg");
+                        Double target_og = jsonObject.getDouble("target_og");
+                        Double ebc = jsonObject.getDouble("ebc");
+                        Double srm = jsonObject.getDouble("srm");
+                        Double ph = jsonObject.getDouble("ph");
+                        Double attenuation_level = jsonObject.getDouble("attenuation_level");
+
+                        JSONArray jsonIngredientMalt = jsonObject.getJSONObject("ingredient").getJSONArray("malt");
+                        String ingedients_malt = new String("");
+                        for(int k=0 ; k<jsonIngredientMalt.length(); k++) {
+                            ingedients_malt += jsonIngredientMalt.getJSONObject(k).getString("name") + "\n";
                         }
-                        catch(JSONException e){
-                            e.printStackTrace();
+                        ingedients_malt = ingedients_malt.substring(0, ingedients_malt.length()-2);
+
+                        JSONArray jsonIngredientHops = jsonObject.getJSONObject("ingredient").getJSONArray("hops");
+                        String ingedients_hops = new String("");
+                        for(int k=0 ; k<jsonIngredientHops.length(); k++) {
+                            ingedients_hops += jsonIngredientHops.getJSONObject(k).getString("name") + "\n";
                         }
+                        ingedients_hops = ingedients_hops.substring(0, ingedients_hops.length()-2);
+
+                        String ingedients_yeast = jsonObject.getString("ingedients_yeast");
+                        String brewers_tips = jsonObject.getString("brewers_tips");
+                        String contibutor = jsonObject.getString("contibutor");
+
+                        beerList.add(new Beer(id,name,tagline,first_brewed,description,image_url,abv,ibu,target_fg,target_og,ebc,srm,ph,attenuation_level,ingedients_malt,ingedients_hops,ingedients_yeast,brewers_tips,contibutor));
                     }
-                });
+                }
+                catch(JSONException e){
+                    e.printStackTrace();
+                    beerList.add(beer);
+                }
+            }
+        }, null);
     }
-
-
 }
 
 
--- app/src/main/java/polybeer/polybeer/Object/Beer.java
+++ app/src/main/java/polybeer/polybeer/Object/Beer.java
@@ -19,20 +19,13 @@ public class Beer {
     private Double srm;
     private Double ph;
     private Double attenuation_level;
-    private Integer volume;
-    private Integer boil_volume;
-    private Integer mash_temperature;
-    private Integer mash_duration;
-    private Integer ferm_temperature;
-    private String twist;
     private String ingedients_malt;
     private String ingedients_hops;
     private String ingedients_yeast;
-    private String food_pairing;
     private String brewers_tips;
     private String contibutor;
 
-    public Beer(Integer id, String name, String tagline, String first_brewed, String description, String image_url, Double abv, Double ibu, Double target_fg, Double target_og, Double ebc, Double srm, Double ph, Double attenuation_level, Integer volume, Integer boil_volume, Integer mash_temperature, Integer mash_duration, Integer ferm_temperature, String twist, String ingedients_malt, String ingedients_hops, String ingedients_yeast, String food_pairing, String brewers_tips, String contibutor) {
+    public Beer(Integer id, String name, String tagline, String first_brewed, String description, String image_url, Double abv, Double ibu, Double target_fg, Double target_og, Double ebc, Double srm, Double ph, Double attenuation_level, String ingedients_malt, String ingedients_hops, String ingedients_yeast, String brewers_tips, String contibutor) {
         this.id = id;
         this.name = name;
         this.tagline = tagline;
@@ -47,21 +40,32 @@ public class Beer {
         this.srm = srm;
         this.ph = ph;
         this.attenuation_level = attenuation_level;
-        this.volume = volume;
-        this.boil_volume = boil_volume;
-        this.mash_temperature = mash_temperature;
-        this.mash_duration = mash_duration;
-        this.ferm_temperature = ferm_temperature;
-        this.twist = twist;
         this.ingedients_malt = ingedients_malt;
         this.ingedients_hops = ingedients_hops;
         this.ingedients_yeast = ingedients_yeast;
-        this.food_pairing = food_pairing;
         this.brewers_tips = brewers_tips;
         this.contibutor = contibutor;
     }
 
     public Beer(){
-
+        this.id = 1;
+        this.name = "Buzz";
+        this.tagline = "A Real Bitter Experience.";
+        this.first_brewed = "09/2007";
+        this.description = "A light, crisp and bitter IPA brewed with English and American hops. A small batch brewed only once.";
+        this.image_url = "https://images.punkapi.com/v2/keg.png";
+        this.abv = 4.5;
+        this.ibu = 60.0;
+        this.target_fg = 1010.0;
+        this.target_og = 1044.0;
+        this.ebc = 20.0;
+        this.srm = 10.0;
+        this.ph = 4.4;
+        this.attenuation_level = 75.0;
+        this.ingedients_malt = "Maris Otter Extra Pale \\n Caramalt \\n Munich";
+        this.ingedients_hops = "Fuggles \\n First Gold \\n Fuggles \\n First Gold \\n Cascade";
+        this.ingedients_yeast = "Wyeast 1056 - American Ale™";
+        this.brewers_tips = "The earthy and floral aromas from the hops can be overpowering. Drop a little Cascade in at the end of the boil to lift the profile with a bit of citrus.";
+        this.contibutor = "Sam Mason <samjbmason>";
     }
 }
